16833dc074644f28191a3931ca338cf80e97530d
Nagaseatuhiro <t236068@st.yamagata-u.ac.jp>
2024-10-18 19:28:00 +0900

    Add files via upload

---
+++ No3kadai.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int k;
+    int j;
+    printf("How meny O K O M E");
+    scanf("%d", &k);
+    
+    printf("How meny P A N N");
+    scanf("%d", &j);
+    
+    
+    printf("O K O M E + P A N N = %d\n", k+j);
+
+    return 0;
+}
